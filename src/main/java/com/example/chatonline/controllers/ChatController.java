d6305911018a27cd0d1669b545a15eb0588d1125
Bruna <brunadelmouro@gmail.com>
2022-03-16 00:24:45 -0300

    implementando funções para ChatController

--- src/main/java/com/example/chatonline/controllers/ChatController.java
+++ src/main/java/com/example/chatonline/controllers/ChatController.java
@@ -5,10 +5,9 @@ import com.example.chatonline.domain.Usuario;
 import com.example.chatonline.services.ChatService;
 import com.example.chatonline.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,23 +16,35 @@ import java.util.List;
 public class ChatController {
 
     @Autowired
-    private ChatService service;
+    private ChatService chatService;
 
     @Autowired
     UsuarioService usuarioService;
 
-    @RequestMapping(value = "/usuarios")
+    @GetMapping(value = "/usuarios")
     public ResponseEntity<List<Usuario>> findOnlineUsers(){
         List<Usuario> usuarios = usuarioService.findByOnlineUsers();
 
         return ResponseEntity.ok().body(usuarios);
     }
 
-    @RequestMapping(value = "/mensagens")
+    @GetMapping(value = "/mensagens")
     public ResponseEntity<List<Mensagem>> findMessages(){
-        List<Mensagem> obj = service.findMessages();
+        List<Mensagem> obj = chatService.findMessages();
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<Void> insertMessage(@RequestBody Mensagem obj){
+        obj = chatService.insertMessage(obj);
 
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @RequestMapping(value = "/mensagens/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteMessage(@PathVariable Integer id){
+        chatService.deleteMessage(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/example/chatonline/services/ChatService.java
+++ src/main/java/com/example/chatonline/services/ChatService.java
@@ -1,20 +1,40 @@
 package com.example.chatonline.services;
 
 import com.example.chatonline.domain.Mensagem;
+import com.example.chatonline.domain.Usuario;
 import com.example.chatonline.repositories.ChatRepository;
 import com.example.chatonline.repositories.MensagemRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ChatService {
 
     @Autowired
-    private MensagemRepository repo;
+    private MensagemRepository mensagemRepository;
 
     public List<Mensagem> findMessages(){
-        return repo.findAll();
+        return mensagemRepository.findAll();
+    }
+
+    public Mensagem findMessage(Integer id){
+        Optional<Mensagem> obj = mensagemRepository.findById(id);
+        return obj.orElseThrow(() ->  new ObjectNotFoundException(1, "Não encontrado"));
+    }
+
+    public Mensagem insertMessage(Mensagem obj){
+       return mensagemRepository.save(obj);
+    }
+
+    public void deleteMessage(Integer id){
+        Mensagem msg = this.findMessage(id);
+        if (msg == null)
+            throw new ObjectNotFoundException(1, "Não encontrado");
+
+        mensagemRepository.deleteById(id);
     }
 }
